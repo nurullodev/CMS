e5d7a5865d068a2cb0252a50c93c2c34801491a2
Nurullo <ghostone234qwer.gmail.com>
2023-08-10 22:32:52 +0500

    add response in service class library

--- CMS.Service/Mappers/MappingProfile.cs
+++ CMS.Service/Mappers/MappingProfile.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using CMS.Service.DTOs.Users;
+using CMS.Service.DTOs.Colors;
+using CMS.Service.DTOs.Damens;
+using CMS.Service.DTOs.Designs;
+using CMS.Domain.Entities.Users;
+using CMS.Service.DTOs.FontSizes;
+using CMS.Service.DTOs.TimeZones;
+using CMS.Domain.Entities.Domains;
+using CMS.Service.DTOs.UserGroups;
+using CMS.Domain.Entities.Designs;
+using CMS.Service.DTOs.DesignTools;
+using CMS.Domain.Entities.TimeZones;
+using CMS.Domain.Entities.DesignTools;
+using CMS.Service.DTOs.DesignCategories;
+using CMS.Domain.Entities.DesignCategories;
 
-namespace CMS.Service.Mappers
+namespace CMS.Service.Mappers;
+
+public class MappingProfile : Profile 
 {
-    internal class MappingProfile
+    public MappingProfile()
     {
+        //User
+        CreateMap<UserResultDto, User>().ReverseMap();
+        CreateMap<UserUpdateDto, User>().ReverseMap();
+        CreateMap<User, UserCreationDto>().ReverseMap();
+
+        //userGroup 
+        CreateMap<UserGroupResultDto, UserGroup>().ReverseMap();
+        CreateMap<UserGroupUpdateDto, UserGroup>().ReverseMap();
+        CreateMap<UserGroup, UserGroupCreationDto>().ReverseMap();
+        
+        //Damen
+        CreateMap<DamenResultDto, Damen>().ReverseMap();
+        CreateMap<DamenUpdateDto, Damen>().ReverseMap();    
+        CreateMap<Damen, DamenCreationDto>().ReverseMap();
+
+        //DesignCategory 
+        CreateMap<DesignCategoryResultDto, DesignCategory>().ReverseMap();
+        CreateMap<DesignCategoryUpdateDto, DesignCategory>().ReverseMap();  
+        CreateMap<DesignCategory, DesignCategoryCreationDto>().ReverseMap();    
+
+        //Design 
+        CreateMap<DesignResultDto, Design>().ReverseMap();
+        CreateMap<DesignUpdateDto, Design>().ReverseMap();
+        CreateMap<Design, DesignCreationDto>().ReverseMap();
+
+        //DesignTool
+        CreateMap<DesignToolResultDto, DesignTool>().ReverseMap();
+        CreateMap<DesignToolUpdateDto, DesignTool>().ReverseMap();  
+        CreateMap<DesignCategory, DesignCategoryCreationDto>().ReverseMap();
+
+        //Color
+        CreateMap<ColorResultDto, Color>().ReverseMap();    
+        CreateMap<ColorUpdateDto, Color>().ReverseMap();
+        CreateMap<Color, ColorCreationDto>().ReverseMap();
+
+        //TimeZon
+        CreateMap<TimeZonResultDto, TimeZon>().ReverseMap();
+        CreateMap<TimeZonUpdateDto, TimeZon>().ReverseMap();
+        CreateMap<TimeZon, TimeZonCreationDto>().ReverseMap();
+
+        //FontSize 
+        CreateMap<FontSizeResultDto, FontSize>().ReverseMap();  
+        CreateMap<FontSizeUpdateDto, FontSize>().ReverseMap();
+        CreateMap<FontSize, FontSizeCreationDto>().ReverseMap();
     }
 }
