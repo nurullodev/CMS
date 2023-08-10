ae97f80ab6859580f6afbda086ef41726637d6e5
Nurullo <ghostone234qwer.gmail.com>
2023-08-10 08:53:09 +0500

    Change enable in disable all libraries

--- CMS.Domain/Commons/Auditable.cs
+++ CMS.Domain/Commons/Auditable.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CMS.Domain.Commons;
 
-namespace CMS.Domain.Commons
+public abstract class Auditable
 {
-    internal class Auditable
-    {
-    }
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
---
+++ CMS.Domain/Entities/Users/User.cs
@@ -0,0 +1,9 @@
+﻿namespace CMS.Domain.Commons.Entities.Users;
+
+public class User : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
