049daeb12ffcedf71eddae368fc4b6a242af4ff0
Nurullo <ghostone234qwer.gmail.com>
2023-08-10 10:37:56 +0500

    add new pages ef.Npgsql and ef.tools with help nuget

---
+++ CMS.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.DbContext
+{
+    internal class AppDbContext
+    {
+    }
+}
