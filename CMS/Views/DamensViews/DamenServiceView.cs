82526687b0bc7b55de3e0daa9f0c4ac83f99065a
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-13 17:38:47 +0500

    Views Servise Optimised

--- CMS/Views/DamensViews/DamenServiceView.cs
+++ CMS/Views/DamensViews/DamenServiceView.cs
@@ -11,12 +11,12 @@ public class DamenServiceView
 
     public async void Methods()
     {
-        Console.WriteLine("1. Create\n" +
-            "2. Update\n" +
-            "3. get by Id\n" +
-            "4. Delete \n" +
-            "5. Get All\n" +
-            "6. Dashboard");
+        Console.WriteLine(@"1. Create
+            2.Update
+            3. get by Id 
+            4. Delete 
+            5. Get All
+            6. Dashboard");
         Console.Write(">>>>> ");
         string number = Console.ReadLine();
         switch (number)
